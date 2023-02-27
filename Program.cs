fe9c262e8561003491c28b752796aae5447c610c
Ausha M S <anusgowda050@gmail.com>
2023-02-27 17:23:43 +0530

    Using Join Method of Thread Class

--- Program.cs
+++ Program.cs
@@ -280,17 +280,17 @@ namespace ThreadingDemo
 
 
 //using Sleep method
-class Program
+/*class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Main Thread Started");
-        Thread thread1 = new Thread(Method1);
-        Thread thread2 = new Thread(Method2);
-        Thread thread3 = new Thread(Method3);
-        thread1.Start();
-        thread2.Start();
-        thread3.Start();
+        Thread th1 = new Thread(Method1);
+        Thread th2 = new Thread(Method2);
+        Thread th3 = new Thread(Method3);
+        th1.Start();
+        th2.Start();
+        th3.Start();
         Console.WriteLine("Main Thread Ended");
         Console.Read();
     }
@@ -298,6 +298,47 @@ class Program
     {
         Console.WriteLine("Method1 - Thread1 Started");
         Thread.Sleep(3000);
+        Console.WriteLine("Method1 - Thread 1 Ended");
+    }
+    static void Method2()
+    {
+        Console.WriteLine("Method2 - Thread2 Started");
+        Thread.Sleep(2000);
+        Console.WriteLine("Method2 - Thread2 Ended");
+    }
+    static void Method3()
+    {
+        Console.WriteLine("Method3 - Thread3 Started");
+        Thread.Sleep(5000);
+        Console.WriteLine("Method3 - Thread3 Ended");
+    }
+}*/
+
+//Using Join Method of Thread Class
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Main Thread Started");
+        Thread th1 = new Thread(Method1);
+        Thread th2 = new Thread(Method2);
+        Thread th3 = new Thread(Method3);
+
+        th1.Start();
+        th2.Start();
+        th3.Start();
+
+        th1.Join();
+        th2.Join();
+        th3.Join();
+
+        Console.WriteLine("Main Thread Ended");
+    }
+
+    static void Method1()
+    {
+        Console.WriteLine("Method1 - Thread1 Started");
+        Thread.Sleep(1000);
         Console.WriteLine("Method1 - Thread 1 Ended");
     }
     static void Method2()
