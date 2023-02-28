7e73897905c75250d9d7848a731fbfbfa07ddaf7
Ausha M S <anusgowda050@gmail.com>
2023-02-28 11:38:59 +0530

    Mutex class with destructors.

--- Program.cs
+++ Program.cs
@@ -766,8 +766,8 @@ namespace ThreadingDemo
     }
 }*/
 
-//Mutex class
-class Program
+//Mutex class with Constructors.
+/*class Program
     {
     private static Mutex mutex = new Mutex();
     static void Main(string[] args)
@@ -796,5 +796,50 @@ class Program
         {
             mutex.ReleaseMutex();
         }
+    }
+}*/
+
+//Mutex class with destructors.
+class Program
+{
+    private static Mutex mutex = new Mutex();
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            Thread threadObj = new Thread(MutexDemo)
+            {
+                Name = "Thread " + i
+            };
+            threadObj.Start();
+        }
+        Console.ReadKey();
+    }
+
+    static void MutexDemo()
+    {
+        Console.WriteLine(Thread.CurrentThread.Name + " Wants to Enter Critical Section for processing");
+        if (mutex.WaitOne(1000))
+        {
+            try
+            {
+                Console.WriteLine("Success: " + Thread.CurrentThread.Name + " is Processing now");
+                Thread.Sleep(2000);
+                Console.WriteLine("Exit: " + Thread.CurrentThread.Name + " is Completed its task");
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+                Console.WriteLine(Thread.CurrentThread.Name + " Has Released the mutex");
+            }
+        }
+        else
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " will not acquire the mutex");
+        }
+    }
+    ~Program()
+    {
+        mutex.Dispose();
     }
 }
